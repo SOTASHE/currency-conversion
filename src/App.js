9c21402a2e5029587860bc91e7f784c249710277
Snaye Sotashe <sinayesotatshe@gmail.com>
2022-07-15 23:03:33 +0200

    Reading data with useEffect hook, updating inputs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,109 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import CurrencyRow from './CurrencyRow';
 
+const requestURL = "https://api.exchangerate.host/latest?base=ZAR";
+
 function App() {
-  return (
-    <>
-      <h1>Convert</h1>
-      <CurrencyRow />
-      <div className='equals'>=</div>
-      <CurrencyRow />
-    </>
-  );
-}
+
+
+  const [currency, setCurrency] = useState([]);
+
+  //from currency to base currency
+  const [fromCurrency, setFromCurrency] = useState();
+  //to currency
+  const [toCurrency, setToCurrency] = useState();
+  //exchange rate
+  const [exchangeRate, setExchangeRate] = useState(); 
+  
+  console.log(exchangeRate);
+  //amount to convert
+  const [amount, setAmount] = useState(1);
+  //converted amount
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [amountIntFromCurrency, setAmountIntFromCurrency] = useState(true);
+  
+  // console.log(currency);
+
+  //const [base, setBase] = useState("ZAR");
+  //update currency every 15 minutes
+  //const [interval, setInterval] = useState(15 * 60 * 1000);
+  //const [error, setError] = useState(null);
+
+  let toAmount, fromAmount;
+  if(amountIntFromCurrency){
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  }
+  else {
+    fromAmount = amount / exchangeRate;
+    toAmount = amount;
+  }
+
+
+  useEffect(() => {
+
+    fetch(requestURL)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data);
+        setCurrency([data.base, ...Object.keys(data.rates)]);
+
+        setFromCurrency(data.base);
+        setToCurrency(Object.keys(data.rates)[0]);
+
+        setExchangeRate(data.rates[Object.keys(data.rates)[0]]);
+        //setBase(data.base);
+        //setInterval(15 * 60 * 1000);
+
+
+
+      }
+    );
+      }, []);
+    
+    
+    
+    
+    return (
+      <>
+        <h1>Convert</h1>
+        <CurrencyRow
+         
+          currency={currency}
+
+          selectedCurrency={fromCurrency}
+          
+          onSelect={(event) => setFromCurrency(event.target.value)}
+
+          amount={fromAmount}
+          onChangeAmount={(event) => {
+            setAmount(event.target.value);
+            setAmountIntFromCurrency(true);
+          }
+          }
+
+        
+        />
+        <div className='equals'>=</div>
+        <CurrencyRow
+        
+          currency={currency}
+          selectedCurrency={toCurrency}
+         
+          onSelect={e => setToCurrency(e.target.value)}
+          amount={toAmount}
+
+          onChangeAmount={(event) => {
+            setAmount(event.target.value);
+
+            setAmountIntFromCurrency(false);
+          }
+          }
+        />
+      </>
+    );
+  }
 
 export default App;
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow({ currency, selectedCurrency, onSelect,amount,onChangeAmount }) {
   return (
     <div>
-      <input type="number" placeholder="0.00" className='input' />
-      <select>
-        <option value="ZAR">ZAR</option> 
+          <input type="number" placeholder="0.00" className="input" value={amount} onChange={onChangeAmount } />
+      <select value={selectedCurrency} onChange={onSelect}>
+        {/* <option value="ZAR">ZAR</option> 
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="GBP">GBP</option>
         <option value="HKD">HKD</option> 
-        <option value="KES">KES</option>
+        <option value="KES">KES</option> */}
+
+        {currency.map((currency, index) => {
+          return (
+            <option key={index} value={currency}>
+              {currency}
+            </option>
+          );
+        })}
       </select>
     </div>
   );
